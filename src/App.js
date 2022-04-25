b125dcaa12b797f1bb5222cab6f49dbd3a9847c7
Abdelwahab <Abdelwahabahmed93@gmail.com>
2022-04-25 15:47:19 +0200

    ADD Google analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import ReactGA from "react-ga4";
+import { GOOGLE_TRACKING_ID } from "./constants";
 
 function App() {
+  ReactGA.initialize(GOOGLE_TRACKING_ID);
+  ReactGA.send("pageview");
+
   return (
     <div>
       <Main />
---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const GOOGLE_TRACKING_ID = "G-PNVK2LGZM4";
