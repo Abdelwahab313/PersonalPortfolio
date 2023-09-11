1195989b8693404e5399cd07fc8004368a70cd6e
Abdelwahab <Abdelwahabahmed93@gmail.com>
2023-09-11 20:49:02 +0300

    Update Packages

--- src/portfolio.js
+++ src/portfolio.js
@@ -39,7 +39,7 @@ const socialMediaLinks = {
 
 const skillsSection = {
   title: "What I do",
-  subTitle: "CRAZY FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
+  subTitle: "PASSIONATE FULL STACK DEVELOPER WHO WANTS TO EXPLORE EVERY TECH STACK",
   skills: [
     emoji(
       "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
